eebc24d85f5072abf660ff34196eac44870e0d32
wizard7377 <wizardishlike@gmail.com>
2023-02-15 07:02:12 -0500

    Updated example

--- examples/example-config.hpp
+++ examples/example-config.hpp
@@ -3,6 +3,10 @@
 #pragma once
 
 #define BOT_TOKEN "Your-bot-token-here"
-#define MYSQL_CONNECT ""
-#define MYSQL_PASSWORD ""
-#define MYSQL_USERNAME ""
+#define MYOP_HOST ""
+#define MYOP_USER ""
+#define MYOP_PASS ""
+#define MYOP_DATA ""
+#define MYOP_PORT 3306
+#define MYOP_SOCK NULL
+#define MYOP_FLAG 0
